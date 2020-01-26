b67f0b65d6c8e41f3d293e149fc2ab8b0f9ff4c5
Nazareth <scania.patriot@gmail.com>
2020-01-26 14:29:58 +0100

    Added som JS

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -3,8 +3,16 @@
  */
 (function () {
     "use strict";
-
-
+    if(document.title === "Report from the course sections") {
+        displayMessage();
+    }
 
     console.log("All ready.");
 }());
+
+function displayMessage(){
+    let quote = "Non est ad astra mollis e terris via"
+    window.status = quote;
+    document.getElementById("quote_paragraph").innerHTML = "<b>" + quote + "</b>";
+    console.log("Message showed");
+}
