f4e2d5456d530c833c49b7ab62e81981b9640a4d
Nazareth <scania.patriot@gmail.com>
2020-01-27 15:11:24 +0100

    test of fetch

--- work/s03/js/s03.js
+++ work/s03/js/s03.js
@@ -2,19 +2,35 @@
 
 (function () {
     'use strict';
-    getData();
-
+    let jsonData = getData();
+    displayData(jsonData);
 }());
+
+function displayData(jsonData) {
+    jsonData.then(function (response) {
+        let keys = Object.keys(response);
+        console.log(keys);
+
+        keys.forEach(function (item,index) {
+            Object.keys(response[item]).forEach(function (item,index) {
+                document.getElementById("content").innerHTML += index + ":" + item + "<br>";
+            });
+            //document.getElementById("content").innerHTML += index + ":" + item + "<br>";
+        })
+    });
+}
 
 function getData() {
     //fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081')
-    fetch('data/1081.json')
+    let test = fetch('data/1081.json')
         .then((response) => {
             return response.json();
         })
         .then((myJson) => {
             console.log(myJson);
+            return myJson;
         });
 
     console.log('Sandbox MEGA is ready!');
+    return test;
 }
