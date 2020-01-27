f1b8e7c38e5a8244f404923dff66cdfd921bbce5
Nazareth <scania.patriot@gmail.com>
2020-01-27 13:31:06 +0100

    test of fetch

--- work/s03/js/s03.js
+++ work/s03/js/s03.js
@@ -7,8 +7,8 @@
 }());
 
 function getData() {
-    fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081')
-    //fetch('data/1081.json')
+    //fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081')
+    fetch('data/1081.json')
         .then((response) => {
             return response.json();
         })
