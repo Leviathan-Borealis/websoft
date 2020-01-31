067d0a364758732ea95dcdbc97f66897d69f4adb
Nazareth <scania.patriot@gmail.com>
2020-01-31 14:03:41 +0100

    plane as object finished

--- work/js/flysim.js
+++ work/js/flysim.js
@@ -1,168 +1,112 @@
-let plane, button, paused, timer, interval, movePlane;
+let button, aPlane;
 
-let planeObj = {
-    planeElement: document.createElement('img'),
-    rotatePlane: function (deg) {
-        plane.style.transform = "rotate(" + deg + "deg)";
-        plane.style.transition = "transform .8s ease-in-out";
-    },
-    rotate180: function () {
+class PlaneObj {
+    paused = false;
+    timer = 5;
+    interval = {};
+    directionX = 1;
+    directionY = 0;
+    planeElement = {};
 
-    },
-    showPlane: function () {
-        plane.style.left = (Math.floor(Math.random() * (areaWidth - plane.width))) + 'px';
-        plane.style.top = (Math.floor(Math.random() * (areaHeight - plane.height))) + 'px';
-        plane.style.transform = "rotate(" + 90 + "deg)";
-        area.appendChild(plane);
-        interval = window.setInterval(movePlane, timer);
-    },
-    freezeAndHidePlane: function () {
+    constructor() {
+        this.planeElement = document.createElement('img');
+        this.planeElement.src='/websoft/work/js/flygplan.png';
+        this.planeElement.style.position ='absolute';
+        this.planeElement.style.left = '0px';
+        this.planeElement.style.top = '0px';
+        this.planeElement.style.zIndex = '10000';
+        this.planeElement.style.transform = "rotate(" + 0 + "deg)";
+        this.planeElement.addEventListener('click', this.turnPlane);
+        console.log("Constructor " + this.planeElement);
+    }
 
-    },
-    movePlaneObj: function () {
+    rotatePlane (deg) {
+        this.planeElement.style.transform = "rotate(" + deg + "deg)";
+        this.planeElement.style.transition = "transform .8s ease-in-out";
+    }
 
-        this.planeElement.style.left = Number.parseInt(this.planeElement.style.left.substr(0, this.planeElement.style.left.length - 2)) + dirX + "px";
-        plane.style.top = Number.parseInt(plane.style.top.substr(0, plane.style.top.length - 2)) + dirY + "px";
+    turnPlane () {
+        if(aPlane.directionX === 0){
+            aPlane.directionX = (Math.random() < 0.5)?-1:1;
+            aPlane.directionY = 0;
+            aPlane.rotatePlane((aPlane.directionX > 0) ? 90 : 270);
+        } else {
+            aPlane.directionY = (Math.random() < 0.5)?-1:1;
+            aPlane.directionX = 0;
+            aPlane.rotatePlane((aPlane.directionY < 0) ? 0 : 180);
+        }
+    }
 
+    showPlane () {
+        this.planeElement.style.left = (Math.floor(Math.random() * (window.innerWidth - this.planeElement.width))) + 'px';
+        this.planeElement.style.top = (Math.floor(Math.random() * (window.innerHeight - this.planeElement.height))) + 'px';
+        this.planeElement.style.transform = "rotate(" + 90 + "deg)";
+        document.body.appendChild(this.planeElement);
+        console.log("showPlane " + this.planeElement);
+        this.interval = window.setInterval(this.movePlaneObj, this.timer);
+    }
 
-        if (Number.parseInt(plane.style.left.substr(0, plane.style.left.length - 2)) <= 0) {
-            if(dirX < 0) {
-                this.rotatePlane(90);
-                dirX *= -1;
+    freezeAndHidePlane () {
+        if(!this.paused) {
+            window.clearInterval(this.interval);
+            document.body.removeChild(this.planeElement);
+            this.paused = true;
+        } else {
+            this.interval = window.setInterval(this.movePlaneObj,this.timer);
+            document.body.appendChild(this.planeElement);
+            this.paused = false;
+        }
+    }
+
+    movePlaneObj () {
+        console.log("movePlaneObj " + aPlane.planeElement);
+        aPlane.planeElement.style.left = Number.parseInt(aPlane.planeElement.style.left.substr(0, aPlane.planeElement.style.left.length - 2)) + aPlane.directionX + "px";
+        aPlane.planeElement.style.top = Number.parseInt(aPlane.planeElement.style.top.substr(0, aPlane.planeElement.style.top.length - 2)) + aPlane.directionY + "px";
+
+        if (Number.parseInt(aPlane.planeElement.style.left.substr(0, aPlane.planeElement.style.left.length - 2)) <= 0) {
+            if(aPlane.directionX < 0) {
+                aPlane.rotatePlane(90);
+                aPlane.directionX *= -1;
             }
-        } else if (Number.parseInt(plane.style.left.substr(0, plane.style.left.length - 2)) + plane.width >= areaWidth) {
-            if(dirX > 0) {
-                this.rotatePlane(270);
-                dirX *= -1;
+        } else if (Number.parseInt(aPlane.planeElement.style.left.substr(0, aPlane.planeElement.style.left.length - 2)) + aPlane.planeElement.width >= window.innerWidth) {
+            if(aPlane.directionX > 0) {
+                aPlane.rotatePlane(270);
+                aPlane.directionX *= -1;
             }
-        } else if (Number.parseInt(plane.style.top.substr(0, plane.style.top.length - 2)) <= 0) {
-            if(dirY < 0) {
-                this.rotatePlane(180);
-                dirY *= -1;
+        } else if (Number.parseInt(aPlane.planeElement.style.top.substr(0, aPlane.planeElement.style.top.length - 2)) <= 0) {
+            if(aPlane.directionY < 0) {
+                aPlane.rotatePlane(180);
+                aPlane.directionY *= -1;
             }
-        } else if (Number.parseInt(plane.style.top.substr(0, plane.style.top.length - 2)) + plane.height >= areaHeight) {
-            if(dirY > 0) {
-                this.rotatePlane(0);
-                dirY *= -1;
+        } else if (Number.parseInt(aPlane.planeElement.style.top.substr(0, aPlane.planeElement.style.top.length - 2)) + aPlane.planeElement.height >= window.innerHeight) {
+            if(aPlane.directionY > 0) {
+                aPlane.rotatePlane(0);
+                aPlane.directionY *= -1;
             }
         }
     }
-};
+}
 
 (function () {
     'use strict';
-
-    plane = document.createElement('img');
     button = document.createElement("button");
-    paused = false;
-    timer = 5;
-
     flySim();
-
 }());
 
-
-
 function flySim() {
-    let area = document.body,
-        areaHeight = window.innerHeight,
-        areaWidth = window.innerWidth,
-        dirX = 1,
-        dirY = 0;
-
+    aPlane = new PlaneObj();
 
     button.addEventListener("click", pauseAndHide);
     button.style.position ='absolute';
     button.className = "plane_button";
     button.textContent = "Show/Hide plane";
-    button.style.zIndex = 10000;
-    area.appendChild(button);
+    button.style.zIndex = "10000";
+    document.body.appendChild(button);
 
-    /**
-     * Set the attributes for the plane
-     **/
-    plane.src='/websoft/work/js/flygplan.png';
-    plane.style.position ='absolute';
-    plane.style.left = '0px';
-    plane.style.top = '0px';
-    plane.style.zIndex = '10000';
-    plane.style.transform = "rotate(" + 0 + "deg)";
-    plane.addEventListener('click', changeCardinal);
-
-    function rotatePlaneTo(deg) {
-        plane.style.transform = "rotate(" + deg + "deg)";
-        plane.style.transition = "transform .8s ease-in-out";
-    }
-
-    function changeCardinal() {
-        if(dirX === 0){
-            dirX = (Math.random() < 0.5)?-1:1;
-            dirY = 0;
-            rotatePlaneTo((dirX > 0) ? 90 : 270);
-        } else {
-            dirY = (Math.random() < 0.5)?-1:1;
-            dirX = 0;
-            rotatePlaneTo((dirY < 0) ? 0 : 180);
-        }
-    }
-
-
-    /**
-     * A function for displaying the plane in random positions
-     **/
-    function spawnPlane() {
-        let newX = Math.floor(Math.random() * (areaWidth - plane.width)),
-            newY = Math.floor(Math.random() * (areaHeight - plane.height));
-
-        plane.style.left = newX+'px';
-        plane.style.top = newY+'px';
-        plane.style.transform = "rotate(" + 90 + "deg)";
-        area.appendChild(plane);
-
-        interval = window.setInterval(movePlane, timer);
-    }
-
-    movePlane = function () {
-
-        plane.style.left = Number.parseInt(plane.style.left.substr(0, plane.style.left.length - 2)) + dirX + "px";
-        plane.style.top = Number.parseInt(plane.style.top.substr(0, plane.style.top.length - 2)) + dirY + "px";
-
-
-        if (Number.parseInt(plane.style.left.substr(0, plane.style.left.length - 2)) <= 0) {
-            if(dirX < 0) {
-                rotatePlaneTo(90);
-                dirX *= -1;
-            }
-        } else if (Number.parseInt(plane.style.left.substr(0, plane.style.left.length - 2)) + plane.width >= areaWidth) {
-            if(dirX > 0) {
-                rotatePlaneTo(270);
-                dirX *= -1;
-            }
-        } else if (Number.parseInt(plane.style.top.substr(0, plane.style.top.length - 2)) <= 0) {
-            if(dirY < 0) {
-                rotatePlaneTo(180);
-                dirY *= -1;
-            }
-        } else if (Number.parseInt(plane.style.top.substr(0, plane.style.top.length - 2)) + plane.height >= areaHeight) {
-            if(dirY > 0) {
-                rotatePlaneTo(0);
-                dirY *= -1;
-            }
-        }
-    };
-
-    spawnPlane();
+    aPlane.showPlane();
 }
 
 function pauseAndHide() {
-    if(!paused) {
-        window.clearInterval(interval);
-        document.body.removeChild(plane);
-        paused = true;
-    } else {
-        interval = window.setInterval(movePlane,timer);
-        document.body.appendChild(plane);
-        paused = false;
-    }
+    aPlane.freezeAndHidePlane();
 }
 
