490c43c74686330814bf65fcdd47b24e7d605520
Nazareth <scania.patriot@gmail.com>
2020-02-10 10:32:25 +0100

    First asp commit

---
+++ work/asp/testApp01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testApp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
